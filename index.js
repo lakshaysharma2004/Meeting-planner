88b495db044dada5657c641fd423eb409e1d04f4
Lakshay Sharma <lakshay.mris2004@gmail.com>
2024-06-22 22:33:00 +0530

    Done till adding new topics

---
+++ index.js
@@ -0,0 +1,70 @@
+var meetingTopics = [];
+
+var defaultValues = {
+    time: 4,
+    title: "Default",
+    status: "Incomplete"
+};
+
+function populatemeetingitems(){
+    clearTopic()
+    meetingTopics.forEach((topic,index ) => {
+        document.getElementById("contentRow").appendChild(generateElements(topic,index ));
+    })
+}
+
+function addTopic() {
+    meetingTopics.push({})
+    populatemeetingitems();
+}
+
+function clearTopic(){
+    document.getElementById("contentRow").innerHTML = ''
+}
+    
+function deleteTopic() {
+    meetingTopics.splice(index, 1);
+    populatemeetingitems();
+}
+
+function finishTopic() {
+    console.log("Topic Finished")
+}
+
+function startMeeting() {
+    console.log("Meeting Started")
+}
+
+function generateElements(topic,index) {
+    var wrapper = document.createElement("div")
+    console.log(wrapper);
+
+    // create timer
+    var topicTimer = document.createElement("span")
+    wrapper.appendChild(topicTimer)
+
+    // create done button
+    var finishTopic = document.createElement("button")
+    wrapper.appendChild(finishTopic)
+    finishTopic.textContent = "Done";
+
+    // create topic title
+    var topicTitle = document.createElement("input")
+    wrapper.appendChild(topicTitle)
+
+    // create topic duration
+    var topicDuration = document.createElement("input")
+    wrapper.appendChild(topicDuration)
+
+    // create delete button
+    var deleteButton = document.createElement("button");
+    wrapper.appendChild(deleteButton);
+    deleteButton.textContent = "del";
+    deleteButton.onclick = function() { deleteTopic(index); };
+    return wrapper
+}
+
+
+
+
+
