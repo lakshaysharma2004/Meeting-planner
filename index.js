fb09bb2c00434c447d88db0c74ea17c2f546fa62
Lakshay Sharma <lakshay.mris2004@gmail.com>
2024-06-25 23:42:58 +0530

    added delete functionality

--- index.js
+++ index.js
@@ -7,66 +7,65 @@ var defaultValues = {
 };
 
 function populatemeetingitems(){
-    clearTopic()
-    meetingTopics.forEach((topic,index ) => {
-        document.getElementById("contentRow").appendChild(generateElements(topic,index ));
-    })
+    clearTopic();
+    meetingTopics.forEach((topic, index) => {
+        document.getElementById("contentRow").appendChild(generateElements(topic, index));
+    });
 }
 
 function addTopic() {
-    meetingTopics.push({})
+    meetingTopics.push({});
     populatemeetingitems();
 }
 
 function clearTopic(){
-    document.getElementById("contentRow").innerHTML = ''
+    document.getElementById("contentRow").innerHTML = '';
 }
-    
-function deleteTopic() {
-    meetingTopics.splice(index, 1);
-    populatemeetingitems();
+
+function deleteTopic(index) {
+    meetingTopics.splice(index, 1); // Remove the item from the array
+    populatemeetingitems(); // Refresh the displayed list
 }
 
 function finishTopic() {
-    console.log("Topic Finished")
+    console.log("Topic Finished");
 }
 
 function startMeeting() {
-    console.log("Meeting Started")
+    console.log("Meeting Started");
 }
 
-function generateElements(topic,index) {
-    var wrapper = document.createElement("div")
-    console.log(wrapper);
+function generateElements(topic, index) {
+    var wrapper = document.createElement("div");
+    wrapper.setAttribute("data-index", index);
+    wrapper.classList.add('Table');
 
     // create timer
-    var topicTimer = document.createElement("span")
-    wrapper.appendChild(topicTimer)
+    var topicTimer = document.createElement("span");
+    wrapper.appendChild(topicTimer);
 
     // create done button
-    var finishTopic = document.createElement("button")
-    wrapper.appendChild(finishTopic)
-    finishTopic.textContent = "Done";
-    finishTopic.classList.add('Done')
+    var finishTopicButton = document.createElement("button");
+    wrapper.appendChild(finishTopicButton);
+    finishTopicButton.textContent = "Done";
+    finishTopicButton.classList.add('Done');
 
     // create topic title
-    var topicTitle = document.createElement("input")
-    wrapper.appendChild(topicTitle)
+    var topicTitle = document.createElement("input");
+    wrapper.appendChild(topicTitle);
 
     // create topic duration
-    var topicDuration = document.createElement("input")
-    wrapper.appendChild(topicDuration)
+    var topicDuration = document.createElement("input");
+    wrapper.appendChild(topicDuration);
 
     // create delete button
     var deleteButton = document.createElement("button");
     wrapper.appendChild(deleteButton);
     deleteButton.textContent = "DEL";
-    deleteButton.onclick = function() { deleteTopic(index); };
-    deleteButton.classList.add('delete')
-    return wrapper
+    deleteButton.classList.add('delete');
+    deleteButton.addEventListener('click', function() {
+        deleteTopic(index);
+    });
+
+    return wrapper;
 }
-
-
-
-
-
